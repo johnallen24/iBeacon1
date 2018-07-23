1acc560202ea55aee8631a87efdec859133fbb09
John Allen <johnallen2455@gmail.com>
2018-07-23 13:43:07 -0400

    Initial Commit

---
+++ iBeacon1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBeacon1
+//
+//  Created by John Allen on 7/23/18.
+//  Copyright © 2018 jallen.studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
